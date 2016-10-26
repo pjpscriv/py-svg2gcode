4660d72bf6f0d8249ada0d68005750ae20e8f351
Peter Scriven <justabitdope@gmail.com>
2016-10-26 17:06:15 +1300

    Fixed some scaling bugs. Not all of them. There's still problems converting star.svg for example. Also changed debugging so it outputs to a file.

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 """G-code emitted at the start of processing the SVG file"""
-preamble = "G28\nG1 Z5.0"
+preamble = "G28\nG1 Z0.0\nM05"
 
 """G-code emitted at the end of processing the SVG file"""
 postamble = "G28"
 
 """G-code emitted before processing a SVG shape"""
-shape_preamble = "G4 P200"
+shape_preamble = "G4 P0.2"
 
 """G-code emitted after processing a SVG shape"""
-shape_postamble = "G4 P200"
+shape_postamble = "G4 P0.2\nM05"
 
-# A4 is 210mm x 297mm
+# A4 area:               210mm x 297mm
+# Printer Cutting Area: ~178mm x ~344mm
+# Testing Area:          150mm x 150mm  (for now)
 """Print bed width in mm"""
-bed_max_x = 1000
+bed_max_x = 150 
 
 """Print bed height in mm"""
-bed_max_y = 1000
+bed_max_y = 150
 
 """ Used to control the smoothness/sharpness of the curves.
     Smaller the value greater the sharpness. Make sure the
